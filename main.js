73617b00bfc5a7c8a3518787901c3719ca2b30a4
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-04 01:26:03 +0200

    create function with fs and redirection

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http'); //https, fs, url은 모듈임.
 var fs = require('fs');
 var url = require('url')
+var qs = require('querystring'); // post전송 데이터 수신시 사용
 
 function templateHTML(title,list,body){
 
@@ -15,6 +16,7 @@ function templateHTML(title,list,body){
             <body>
               <h1><a href="/">WEB</a></h1>
               ${list}
+              <a href="/create">create</a>
               ${body}
             </body>
             </html>
@@ -39,7 +41,7 @@ var app = http.createServer(function(request,response){
     
     var queryData = url.parse(_url, true).query;
     var pathname=url.parse(_url, true).pathname
-    
+    console.log(pathname);
     //console.log(queryData.id) // HTML, CSS, ...
     
     if(pathname ==='/'){
@@ -78,7 +80,59 @@ var app = http.createServer(function(request,response){
           })
         })
       }
-  } else {
+  } else if (pathname==='/create'){
+    //pathname이 /가 아닌 /create인 경우
+
+    fs.readdir('./data',function(error, filelist){
+          
+      var title='Web - create'
+      // var list = `
+      //     <ol>
+      //       <li><a href="/?id=HTML">HTML</a></li>
+      //       <li><a href="/?id=CSS">CSS</a></li>
+      //       <li><a href="/?id=JavaScript">JavaScript</a></li>
+      //     </ol>
+      // `
+      var list = templateList(filelist);
+      
+      var template=templateHTML(title,list,`
+      
+        <form action="http://localhost:3000/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p><textarea name="description" placeholder="description"></textarea></p>
+        <p><input type="submit"></p>
+        </form>
+      `);
+      response.writeHead(200);
+      response.end(template)
+  })
+
+  } else if (pathname==='/create_process'){
+  
+    var body='';
+    
+    request.on('data',function(data){
+      body+=data;
+    }); //post방식으로 전송되는 데이터가 많을 때를 대비해서.
+    //특정량을 서버가 수신할 때마다 콜백함수를 호출한다.
+    //그 때마다 data라는 인자를 통해 수신한 정보를 준다.
+    
+    request.on('end',function(){
+      var post = qs.parse(body); // 여기에 원하는 body 정보가 들어있음.
+      var title=post.title;
+      var description=post.description;
+      //이제 받아왔으니 파일을 저장해야함.
+      
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        //이 곳 콜백 함수는 파일의 저장이 끝났을 때에 실행되는 함수.
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      } )
+
+    }); //정보가 조각조각 들어오다, 더 이상 없으면 이 콜백함수를 호출.(request.on('end'))
+    //이 때 정보 수신이 끝났음.
+  }
+  else {
     response.writeHead(404);
     response.end('Not Found');
   } 
